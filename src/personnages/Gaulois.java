372f044daa67ada8477126acf72609799d2c8f0a
MaelaViguier <maela.viguier4@gmail.com>
2024-01-17 09:47:14 +0100

    TP1 fini

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,24 +1,30 @@
 package personnages;
 
 public class Gaulois {
-	private String nom; 
-	private int force; 
-	
+	private String nom;
+	private int force;
+
 	public Gaulois(String nom, int force) {
-		this.nom = nom; 
-		this.force = force; 
+		this.nom = nom;
+		this.force = force;
 	}
-	
+
 	public String getNom() {
-		return nom; 
+		return nom;
 	}
 	
+	public int getForce() {
+		return force;
+	}
+
 	public void parler(String chaine) {
 		System.out.println("Le gaulois " + nom + " : \"" + chaine + "\".");
 	}
-	
 
-	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+	}
+
 //	public static void main(String[] args) {
 //		Gaulois asterix = new Gaulois("Astérix", 8);
 //		System.out.println(asterix);
